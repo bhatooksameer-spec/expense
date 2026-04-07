e712f688a760b5bec5bafd04389e1d358e7996a7
Muhammad Sameer <bhatooksameer@gmail.com>
2026-04-07 16:22:45 +0500

    Refactor add function and implement row editing

--- app.js
+++ app.js
@@ -1,70 +1,41 @@
-var raw = document.getElementById('row')
-function add() {
-    var getName = document.getElementById('Name').value
+var tableRow = document.getElementById('raw')
+var sno = 1
+function addData(){
+    var getName = document.getElementById('name').value
     var getAmount = document.getElementById('amount').value
-    var getType = document.getElementById('option').value
+    var getType = document.getElementById('type').value
 
-    if (getName == '' || getAmount == '' || getType == '') {
-        alert('Plaese fill the field')
+    if(getName == '' || getAmount == '' || getType == ''){
+        alert("Sorry! Please ! Fill the given field")
     }
-    else {
-        raw.innerHTML += `<tr class="list-group-item d-flex justify-content-between align-items-center"><td class="sno">1</td> <td>${getName}</td> <td>${getAmount}</td> <td>${getType}</td> <td class="button"><button class="btn btn-primary">Edit</button><button class="btn btn-danger">Delete</button></td></tr><br>`
+    else{
+      tableRow.innerHTML += `<tr>
+            <td>${sno++}</td>
+            <td>${getName}</td>
+            <td>${getAmount} </td>
+            <td>${getType}</td>
+            <td>
+              <button class="btn btn-success " onclick="editRow(this)">Edit</button>
+              <button class="btn btn-danger " onclick="delRow(this)">Delete</button>
+            </td></tr>`
     }
+     document.getElementById('name').value = ''
+  document.getElementById('amount').value = ''
+  document.getElementById('type').value = ''
 }
-function edit(item){
-    // var updatedValue = prompt('Enter Updated Value')
+
+function editRow(item){
+    var getName = document.getElementById('name').value
+    var getAmount = document.getElementById('amount').value
+    var getType = document.getElementById('type').value
+    var updatedName = prompt("Update Your Name",item.parentNode.parentNode.childNodes[3].textContent)
+    item.parentNode.parentNode.childNodes[3].textContent = updatedName
 }
-this
-console.log(edit(this))
+function delRow(row){
+    if(confirm("Are You Sure You Want To delete It ?")){
+    row.parentNode.parentNode.remove()}
+
+}
 
 
-
-
-// var getList = document.getElementById('list')
-
-// function addTask() {
-//     var getInput = document.getElementById('form12')
-
-//     if (getInput.value == '') {
-//         Swal.fire({
-//             icon: "error",
-//             title: "Oops...",
-//             text: "Please enter some value!"
-//         });
-//     }
-//     else {
-//         getList.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center"> <span> ${getInput.value} </span> <span> <button onclick='editList(this)' type="button" class="btn btn-outline-info" data-mdb-ripple-init data-mdb-ripple-color="dark"> Edit </button> <button onclick='delList(this)' type="button" class="btn btn-outline-danger" data-mdb-ripple-init data-mdb-ripple-color="dark"> Delete </button> </span>  </li>`
-//         getInput.value = ''
-//     }
-
-
-// }
-
-
-// function delList(item) {
-
-//     Swal.fire({
-//         title: "Are you sure?",
-//         text: "You won't be able to revert this!",
-//         icon: "warning",
-//         showCancelButton: true,
-//         confirmButtonColor: "#3085d6",
-//         cancelButtonColor: "#d33",
-//         confirmButtonText: "Yes, delete it!"
-//     }).then((result) => {
-//         if (result.isConfirmed) {
-//             Swal.fire({
-//                 title: "Deleted!",
-//                 text: "Your file has been deleted.",
-//                 icon: "success",
-//             });
-//             item.parentNode.parentNode.remove()
-//         }
-//     });
-// }
-
-
-// function editList(item) {
-//     var updatedValue = prompt("Enter updated value", item.parentNode.parentNode.childNodes[1].textContent)
-//     item.parentNode.parentNode.childNodes[1].textContent = updatedValue
 // }
